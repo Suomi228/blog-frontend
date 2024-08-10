9b54f4185498e3a5948b53aea648da275c806ab0
Suomi228 <bixep345@gmail.com>
2024-08-10 19:41:44 +0300

    full article

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -2,13 +2,15 @@ import Header from "./components/header/Header.jsx";
 import "./index.css";
 import Home from  './pages/home/Home.jsx';
 import AddArticle from "./pages/addArticle/AddArticle.jsx";
+import FullArticle from "./pages/fullArticle/FullArticle.jsx";
 function App() {
   return (
     <>
       <Header/>
       <div className="container">
           {/* <Home/> */}
-          <AddArticle/>
+          {/* <AddArticle/> */}
+          <FullArticle/>
       </div>
     </>
   );
